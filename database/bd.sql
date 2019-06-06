de31faf59d2073b13253f8225dfb1580e62d762a
fernando00-00 <mkmbflw@gmail.com>
2019-06-06 01:28:28 -0500

    script db v2 on fix

--- database/bd.sql
+++ database/bd.sql
@@ -1,45 +1,53 @@
-CREATE DATABASE IF NOT EXISTS deudas;
+DROP DATABASE IF EXISTS deudas;
+CREATE DATABASE deudas;
 
 USE deudas;
 
 CREATE TABLE IF NOT EXISTS administradores( 
-	id INT AUTO_INCREMENT,
-    telefono VARCHAR(10) NOT NULL,
+	telefono VARCHAR(10),
+    nombre VARCHAR(30) NOT NULL,
     pass VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (telefono)
     );
     
 CREATE TABLE IF NOT EXISTS deudores(
-   id INT AUTO_INCREMENT,
-   telefono VARCHAR(10) NOT NULL,
+   telefono VARCHAR(10),
    pass VARCHAR(20) NOT NULL,
    nombre VARCHAR(30) NOT NULL,
-   deuda DOUBLE,
-   PRIMARY KEY (id)
+   adeudo DOUBLE,
+   PRIMARY KEY (telefono)
+);
+
+CREATE TABLE IF NOT EXISTS conceptos(
+   concepto VARCHAR(15),
+   id_administrador VARCHAR(10),
+   monto DOUBLE,
+   PRIMARY KEY (concepto),
+   FOREIGN KEY(id_administrador) REFERENCES administradores(telefono)
 );
 
 CREATE TABLE IF NOT EXISTS deudas(
-   concepto VARCHAR(15) NOT NULL,
-   id_administrador INT,
-   id_deudor INT NOT NULL,
-   adeudo DOUBLE,
-   PRIMARY KEY (concepto),
-   FOREIGN KEY(id_deudor) REFERENCES deudores(id),
-   FOREIGN KEY(id_administrador) REFERENCES administradores(id)
+   referencia INT AUTO_INCREMENT,
+   concepto VARCHAR(15),
+   id_deudor VARCHAR(10),
+   debe DOUBLE,
+   PRIMARY KEY (referencia),
+   FOREIGN KEY(concepto) REFERENCES conceptos(concepto),
+   FOREIGN KEY(id_deudor) REFERENCES deudores(telefono)
 );
 
 CREATE TABLE IF NOT EXISTS pagos(
-   id INT AUTO_INCREMENT,
-   id_administrador INT,
-   id_deudor INT NOT NULL,
-   concepto VARCHAR(15),
+   referencia INT AUTO_INCREMENT,
+   id_administrador VARCHAR(10),
+   id_deudor VARCHAR(10),
+   referencia_deuda INT,
    monto DOUBLE,
    fecha DATE,
-   PRIMARY KEY (id),
-   FOREIGN KEY(id_deudor) REFERENCES deudores(id),
-   FOREIGN KEY(id_administrador) REFERENCES administradores(id),
-   FOREIGN KEY(concepto) REFERENCES deudas(concepto)
+   PRIMARY KEY (referencia),
+   FOREIGN KEY(id_deudor) REFERENCES deudores(telefono),
+   FOREIGN KEY(id_administrador) REFERENCES administradores(telefono),
+   FOREIGN KEY(referencia_deuda) REFERENCES deudas(referencia)
 );
 
-INSERT INTO administradores (telefono, pass) 
-	VALUES ('1234554321','qweRty789');
+INSERT INTO administradores (nombre,telefono, pass) 
+	VALUES ('El del Varo','1234554321','qweRty789');

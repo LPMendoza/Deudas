27b0d6ea250bd136c98f668ca4f0edb21b7a1e26
fernando00-00 <mkmbflw@gmail.com>
2019-06-05 22:37:57 -0500

    script db v2

--- database/bd.sql
+++ database/bd.sql
@@ -2,33 +2,44 @@ CREATE DATABASE IF NOT EXISTS deudas;
 
 USE deudas;
 
+CREATE TABLE IF NOT EXISTS administradores( 
+	id INT AUTO_INCREMENT,
+    telefono VARCHAR(10) NOT NULL,
+    pass VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+    );
+    
 CREATE TABLE IF NOT EXISTS deudores(
    id INT AUTO_INCREMENT,
    telefono VARCHAR(10) NOT NULL,
+   pass VARCHAR(20) NOT NULL,
    nombre VARCHAR(30) NOT NULL,
    deuda DOUBLE,
    PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS deudas(
+   concepto VARCHAR(15) NOT NULL,
+   id_administrador INT,
+   id_deudor INT NOT NULL,
+   adeudo DOUBLE,
+   PRIMARY KEY (concepto),
+   FOREIGN KEY(id_deudor) REFERENCES deudores(id),
+   FOREIGN KEY(id_administrador) REFERENCES administradores(id)
+);
+
 CREATE TABLE IF NOT EXISTS pagos(
    id INT AUTO_INCREMENT,
+   id_administrador INT,
    id_deudor INT NOT NULL,
+   concepto VARCHAR(15),
    monto DOUBLE,
    fecha DATE,
    PRIMARY KEY (id),
-   FOREIGN KEY(id_deudor) REFERENCES deudores(id)
+   FOREIGN KEY(id_deudor) REFERENCES deudores(id),
+   FOREIGN KEY(id_administrador) REFERENCES administradores(id),
+   FOREIGN KEY(concepto) REFERENCES deudas(concepto)
 );
 
-CREATE TABLE IF NOT EXISTS deudas(
-   concepto VARCHAR(30) NOT NULL,
-   id_deudor INT NOT NULL,
-   cantidad DOUBLE,
-   PRIMARY KEY (concepto),
-   FOREIGN KEY(id_deudor) REFERENCES deudores(id)
-);
-
-CREATE TABLE IF NOT EXISTS admins(
-   id INT AUTO_INCREMENT,
-   telefono VARCHAR(30) NOT NULL,
-   pass VARCHAR(16) NOT NULL,
-);
+INSERT INTO administradores (telefono, pass) 
+	VALUES ('1234554321','qweRty789');
